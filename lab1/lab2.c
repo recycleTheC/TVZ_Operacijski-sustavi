e7ea67ac0b5e1ce7eed1e10aeef79e7fa632f33b
Mario Kopjar <mario@debian2.apollo.lan>
2022-10-21 22:32:32 +0200

    Small changes in files

--- lab1/lab2.c
+++ lab1/lab2.c
@@ -1,11 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#include<fcntl.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<unistd.h>
+
+// int open(const char* staza, int zastavice, mode_t dozvole);
+// ssize_t read(int opisnik, void* spremnik, size_t kolicina);
+// ssize_t write(int opisnik, const void* spremnik, size_t kolicina); 
+
 int main(int argc, char* argv[]){
 	
 	if(argc < 4){
 		printf("Nije upisan dovoljan broj parametara!\n");
-		printf("Sintaksa: naredba <buffer> <ulaz> <izlaz>\n");
 		exit(-1);
 	}
 	
@@ -27,26 +35,32 @@ int main(int argc, char* argv[]){
 		exit(-2);
 	}
 
-	FILE* src = fopen(argv[2], "rb");
-	if(src == NULL) {
+	int src = open(argv[2], O_RDONLY); // otvaranje samo za citanje
+	if(src == -1) {
 		printf("Izvorisna datoteka se ne moze otvoriti za citanje!\n");
 		exit(-3);
 	}
+	
+	int zastavice = O_WRONLY | O_CREAT | O_TRUNC;
+	int prava = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
 
-	FILE* dst = fopen(argv[3], "wb");
-	if(dst == NULL){
+	int dst = open(argv[3], zastavice, prava); // otvaranje za pisanje, postavljanje prava
+	if(dst == -1){
 		printf("Odredisna datoteka se ne moze otvoriti za pisanje!\n");
 		exit(-4);
 	}
-	
+
 	printf("Kopiranje datoteke uz buffer velicine %d bajta...\n", bufferSize);	
 
-	while(fread(buffer, bufferSize, 1, src) != 0){
-		fwrite(buffer, bufferSize, 1, dst);
+	while(read(src, buffer, bufferSize) > 0){
+		if(write(dst, buffer, bufferSize) == -1){
+			printf("Greska prilikom pisanja u datoteku! Prekidam rad.\n");
+			exit(-5);
+		}
 	}
 
-	fclose(src);
-	fclose(dst);
+	close(src);
+	close(dst);
 
 	printf("Kopiranje zavreno!\n");
 
