b225cb82867f0f72ce4c5af8ef1685c50966440f
Mario Kopjar <mario@debian2.apollo.lan>
2022-11-12 13:57:51 +0100

    Ispravak skripte lab2

--- lab2/script.sh
+++ lab2/script.sh
@@ -16,7 +16,7 @@ for i in 10 100 1000 10000 100000 #1000000
 do
 echo
 echo === Mjerenje za $i procesa djece ===
-./lab2 $i > output/mjerenje-$i.file
-./parser output/mjerenje-$i.file
+./lab2.out $i > output/mjerenje-$i.file
+./parser.out output/mjerenje-$i.file
 echo ========== Kraj mjerenja ===========
 done
